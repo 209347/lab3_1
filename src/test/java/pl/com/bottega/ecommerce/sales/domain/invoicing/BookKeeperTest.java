6eddd9883b11fa5ead946cfff1eb3056a2db30ac
209347 <209347@edu.p.lodz.pl>
2019-04-10 18:21:10 +0200

    isolation Added requastingInvoiceWithTwoItemsShouldCallCalculateTaxTwice test, reformatted code

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -10,12 +10,12 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BookKeeperTest {
 
-    @Test public void requestingInvoiceWithOneItemShouldReturnInvoiceWithOneItem() {
+    @Test
+    public void requestingInvoiceWithOneItemShouldReturnInvoiceWithOneItem() {
         ClientData client = new ClientData(Id.generate(), "testClient1");
         InvoiceRequest invoiceRequest = new InvoiceRequest(client);
 
@@ -25,13 +25,35 @@ public class BookKeeperTest {
         RequestItem requestItem = new RequestItem(productData, 1, new Money(10));
         invoiceRequest.add(requestItem);
 
-        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-
         TaxPolicy taxPolicy = mock(TaxPolicy.class);
-        when(taxPolicy.calculateTax(ProductType.FOOD, new Money(10))).thenReturn(new Tax(new Money(1), "10%"));
+        when(taxPolicy.calculateTax(ProductType.FOOD, new Money(10)))
+                .thenReturn(new Tax(new Money(1), "10%"));
 
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
         Invoice result = bookKeeper.issuance(invoiceRequest, taxPolicy);
 
         assertThat(result.getItems().size(), Matchers.is(1));
+    }
+
+    @Test
+    public void requastingInvoiceWithTwoItemsShouldCallCalculateTaxTwice() {
+        ClientData client = new ClientData(Id.generate(), "testClient1");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(client);
+
+        ProductData productData = mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.FOOD);
+
+        RequestItem requestItem = new RequestItem(productData, 1, new Money(10));
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(ProductType.FOOD, new Money(10)))
+                .thenReturn(new Tax(new Money(1), "10%"));
+
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        Invoice invoiceResult = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        verify(taxPolicy, times(2)).calculateTax(ProductType.FOOD, new Money(10));
     }
 }
